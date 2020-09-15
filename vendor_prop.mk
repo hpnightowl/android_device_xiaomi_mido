512a4d6ff5ab542330d3ebcee77863f3dc6087eb
GiaSen <giammasena@gmail.com>
2020-09-15 11:18:01 +0200

    mido: Add dalvik props for now
     *nuke when libinit will be supported

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,6 +6,15 @@
 PRODUCT_PRODUCT_PROPERTIES += \
 ro.build.fingerprint=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
 
+# [Temp] Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapmaxfree=8m
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
